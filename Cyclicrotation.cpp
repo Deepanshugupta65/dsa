1cbd41d737ecd8a42cf39137623559790b54749a
Deepanshu gupta <99530550+Deepanshugupta65@users.noreply.github.com>
2022-12-25 17:25:19 +0530

    Create Cyclicrotation.cpp

---
+++ Cyclicrotation.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+void rotatearray(int arr[] ,int n){
+    int i=0,j=n-1;
+    while(i!=j){
+        swap(arr[i],arr[j]);
+        i++;
+    }
+}
+int main(){
+    int arr[] ={1,2,3,4,5};
+    int n = sizeof(arr)/sizeof(arr[0]);
+    rotatearray(arr ,n);
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
