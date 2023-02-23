901c5fd0f353544882a7d50a8b6a3c6ff78e684a
Deepanshu gupta <99530550+Deepanshugupta65@users.noreply.github.com>
2023-02-23 13:19:56 +0530

    Create PowerOfnumber.cpp

---
+++ PowerOfnumber.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a ,b;
+    cin>>a>>b;
+    int res = 1;
+    for(int i =1;i<=b;i++){
+        res = res *a;
+    }
+    cout<<res<<endl;
+    return 0;
+}
