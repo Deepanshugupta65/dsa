6b896b38a6842cf2255b5a752a10e7d883ff7ace
Deepanshu gupta <99530550+Deepanshugupta65@users.noreply.github.com>
2023-02-25 01:54:29 +0530

    Create Recursion.cpp
    Factorial code using recursion

---
+++ Recursion.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int factorial(int n){
+    if(n == 0)
+    return 1;
+    int small = factorial(n-1);
+    int big = n * small;
+    return big;
+        // return n * factorial(n-1);-
+
+}
+int main(){
+    int n;
+    cin>>n;
+    int ans = factorial(n);
+    cout<<ans<<endl;
+}
